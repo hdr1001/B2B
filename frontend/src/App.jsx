641bd54d08901eb6a30d65f961a8be313b52b0d5
hdr1001 <HansDeRooij@Hotmail.com>
2024-06-15 18:17:20 +0200

    Added App/Toolbar to UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
-import Button from '@mui/material/Button';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { 
+  AppBar,
+  Toolbar,
+  Typography,
+  Button
+} from '@mui/material';
 import './App.css';
 
 function App() {
@@ -10,25 +13,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        <AppBar>
+          <Toolbar>
+            <Typography align='left' sx={{ flexGrow: 1 }}>B2B API Hub</Typography>
+          </Toolbar>
+        </AppBar>
         <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
