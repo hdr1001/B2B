d6e28cd4404354c8457806751dd2d8e2ca483406
hdr1001 <HansDeRooij@Hotmail.com>
2024-06-17 21:33:00 +0200

    Changed icon & removed stock css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import {
   Typography,
   Button
 } from '@mui/material';
-import './App.css';
+import { styled } from '@mui/material/styles';
+
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +17,10 @@ function App() {
       <div>
         <AppBar>
           <Toolbar>
-            <Typography align='left' sx={{ flexGrow: 1 }}>B2B API Hub</Typography>
+            <Typography align="left" sx={{ flexGrow: 1 }}>B2B API Hub</Typography>
           </Toolbar>
         </AppBar>
+        <Offset />
         <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
